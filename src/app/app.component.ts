02393265ac87ebc3d7eaafb1dd7dd96ee9c1ff1e
Chris Swanner and Joel Adams <joel@>
2018-03-26 10:23:57 -0700

    make an angular template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  currentFocus: string = "Angular Homework";
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  selectedTask = null;
+
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+
+  priorityColor(currentTask) {
+    if (currentTask.done === true) {
+      return "doneEditing"
+    }
+    if(currentTask.priority === 1) {
+      return "bg-info";
+    } else if (currentTask.priority === 2) {
+      return "bg-warning";
+    } else if (currentTask.priority === 3) {
+      return "bg-danger";
+    }
+  }
+
+  tasks: Task[] = [
+    new Task('Finish ya homework', 1),
+    new Task('Begin thinking about jabascript projects', 2),
+    new Task('Add a readme', 3)
+  ];
+
+  addTask(description, priority) {
+    let newTask = new Task(description, priority);
+    this.tasks.push(newTask);
+  }
 }
